9e05a7db57aa9bd72b20079a24be2776499c7615
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2023-01-20 14:26:43 +0100

    Python exercises

--- string_1.py
+++ string_1.py
@@ -7,7 +7,7 @@ print(solution)
 name = input("What's your name?")
 input(name)
 
-####################################
+#####################################
 greeting = 'Good morming'
 name = input('enter your name')
 solution = greeting + " " + name
