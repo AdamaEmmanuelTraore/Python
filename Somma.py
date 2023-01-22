2b1acded3008c152755e588aa2fb02b34e3dcbfd
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2023-01-22 17:57:35 +0100

    Python exerises

---
+++ Somma.py
@@ -0,0 +1,11 @@
+# SOMMA DI DUE NUMERI DATI / ADDITION DE DEUX NOMBRES DONNÉS (STATIC)
+a = 5
+b = 5
+tot = a + b
+print('La somma tra ' + str(a) + ' e ' + str(b) + ': ' + str(tot))
+
+# (SMART)
+a = input('Inserisci il primo numero:\n')
+b = input('Inserisci il secondo numero:\n')
+tot = int(a) + int(b)
+print('La somma tra ' + str(a) + ' e ' + str(b) + ': ' + str(tot))
